01b8f32d840ebe227ddca4623efb1ce28605dea4
Denis Zomarev <eek@live.ru>
2023-04-16 23:00:02 +0300

    feat: render cards

--- YouTube-client-app/src/app/result-search/result-search-service.service.ts
+++ YouTube-client-app/src/app/result-search/result-search-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 export class ResultSearchServiceService {
   constructor(private http: HttpClient) {}
 
-  public getResultSearch(): any {
+  public getResultSearch(): Observable<any> {
     return this.http.get('assets/response.json');
   }
 }
--- YouTube-client-app/src/app/result-search/result-search.component.ts
+++ YouTube-client-app/src/app/result-search/result-search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ResultSearchServiceService } from './result-search-service.service';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-result-search',
@@ -7,10 +8,10 @@ import { ResultSearchServiceService } from './result-search-service.service';
   styleUrls: ['./result-search.component.scss'],
 })
 export class ResultSearchComponent implements OnInit {
+  public cards$: Observable<any> = this.resultSearchServiceService
+    .getResultSearch()
+    .pipe(map((res) => res.items));
   constructor(private resultSearchServiceService: ResultSearchServiceService) {}
 
-  ngOnInit(): void {
-    const result = this.resultSearchServiceService.getResultSearch();
-    console.log(result);
-  }
+  ngOnInit(): void {}
 }
