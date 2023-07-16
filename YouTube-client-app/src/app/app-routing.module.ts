e20a2a69e84a071b8cb65893d4b5f530d8487d02
Denis Zomarev <eek@live.ru>
2023-07-16 23:48:15 +0300

    feat: 404 page

--- YouTube-client-app/src/app/app-routing.module.ts
+++ YouTube-client-app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResultSearchComponent } from './youtube/components/result-search/result-search.component';
+import { Page404Component } from './core/pages/page404/page404.component';
 
-const routes: Routes = [{ path: '', component: ResultSearchComponent }];
+const routes: Routes = [
+  { path: '', component: ResultSearchComponent },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- YouTube-client-app/src/app/core/core.module.ts
+++ YouTube-client-app/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DropdownSettingsComponent } from './components/dropdown-settings/dropdown-settings.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
-  declarations: [DropdownSettingsComponent, HeaderComponent],
+  declarations: [DropdownSettingsComponent, HeaderComponent, Page404Component],
   imports: [CommonModule, SharedModule],
   exports: [HeaderComponent],
 })
