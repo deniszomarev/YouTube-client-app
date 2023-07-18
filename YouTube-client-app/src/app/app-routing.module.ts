2704a76576fa09c4dbea5d8b58ac74f7f59ed63a
Denis Zomarev <eek@live.ru>
2023-07-18 23:24:26 +0300

    feat: added login page

--- YouTube-client-app/src/app/app-routing.module.ts
+++ YouTube-client-app/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResultSearchComponent } from './youtube/components/result-search/result-search.component';
 import { Page404Component } from './core/pages/page404/page404.component';
+import { LoginComponent } from './core/pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: ResultSearchComponent },
-  { path: '**', component: Page404Component },
   { path: 'login', component: LoginComponent },
+  { path: '**', component: Page404Component },
 ];
 
 @NgModule({
--- YouTube-client-app/src/app/core/core.module.ts
+++ YouTube-client-app/src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { DropdownSettingsComponent } from './components/dropdown-settings/dropdo
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { Page404Component } from './pages/page404/page404.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [DropdownSettingsComponent, HeaderComponent, Page404Component],
+  declarations: [DropdownSettingsComponent, HeaderComponent, Page404Component, LoginComponent],
   imports: [CommonModule, SharedModule],
   exports: [HeaderComponent],
 })
